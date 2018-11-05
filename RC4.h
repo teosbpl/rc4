0b76efb7125b9e413b6cbc62a268aa3625b5c87d
Teodor Buchner <buchner@if.pw.edu.pl>
2018-11-06 00:05:20 +0100

    Added C style linkage

--- RC4.h
+++ RC4.h
@@ -9,8 +9,15 @@
 #ifndef RC4_h
 #define RC4_h
 #include <stdio.h>
+#ifdef __cplusplus
+extern "C" {
+#endif
+    /* Declarations of this file */
+    void RC4(unsigned char*,long , unsigned char* , long ,unsigned char* );
+    /* Function to encrypt data represented in array of char "data" with length represented in dataLen using key which is represented in "Key" with length represented in keyLen, and result will be stored in result */
 
-void RC4(unsigned char*,long , unsigned char* , long ,unsigned char* );
-/* Function to encrypt data represented in array of char "data" with length represented in dataLen using key which is represented in "Key" with length represented in keyLen, and result will be stored in result */
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* RC4_h */
